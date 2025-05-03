5534256fb6c5076694f4cf5460207577655ca6f7
albertjanvanhoek <84067352+albertjanvanhoek@users.noreply.github.com>
2025-05-03 07:11:11 +0200

    Updates from Overleaf

--- filters/pdf2svg.lua
+++ filters/pdf2svg.lua
@@ -1,12 +1,13 @@
--- filters/pdf2svg.lua
+-- Zet .../layers.pdf om naar /figures/layers.svg
 function Image (img)
   local p = img.src
   if p:match("%.pdf$") then
-    -- extensie
-    p = p:gsub("%.pdf$", ".svg")
-    -- docs/figures/...  →  ../figures/...
-    p = p:gsub("^docs/", "../")
+    p = p:gsub("%.pdf$", ".svg")  -- extensie
+    p = p:gsub("^docs/", "/")     -- /figures/...
     img.src = p
   end
+  -- optioneel centreren door een klasse toe te voegen
+  -- img.attributes = img.attributes or {}
+  -- img.attributes["class"] = "center"
   return img
 end
